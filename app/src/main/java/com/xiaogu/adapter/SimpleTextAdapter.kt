34e4857b726859961050b9678fcd102d6cb6f087
gushoubang <gushoubang@dituwuyou.com>
2017-05-19 15:41:10 +0800

    添加上了网络请求图片显示

--- app/src/main/java/com/xiaogu/adapter/SimpleTextAdapter.kt
+++ app/src/main/java/com/xiaogu/adapter/SimpleTextAdapter.kt
@@ -3,10 +3,10 @@ package com.xiaogu.adapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.demo.R
 import com.xiaogu.bean.Meizi
 import kotlinx.android.synthetic.main.item_text.view.*
-
 
 /**
  * Created by xiaogu on 2017/5/18.
@@ -20,11 +20,11 @@ class SimpleTextAdapter(val items: List<Meizi>) : RecyclerView.Adapter<SimpleTex
     override fun onBindViewHolder(holder: SimpleTextAdapter.ViewHolder, position: Int) {
         with(holder?.itemView!!) {
             tv_title.text = items[position].who
+            Glide.with(context).load(items[position].url).into(img)
         }
     }
 
     override fun getItemCount() = items.count()
-
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
